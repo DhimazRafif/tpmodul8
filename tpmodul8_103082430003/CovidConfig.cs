119e95a5f7a3b0a027bde09e3ff7f6fe8ffd9f73
DhimazRafif <hanafidhimaz@gmail.com>
2026-04-25 13:37:34 +0700

    menyelesaikan program dengan runtime configuratiuon

--- tpmodul8_103082430003/CovidConfig.cs
+++ tpmodul8_103082430003/CovidConfig.cs
@@ -1,10 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 
 namespace tpmodul8_103082430003
 {
-    internal class CovidConfig
+    public class CovidConfig
     {
-    }
+        public string satuan_suhu { get; set; }
+        public int batas_hari_demam { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+
+        public static CovidConfig LoadConfigFile()
+        {
+            string filename = "covid_config.json";
+            try
+            {
+                string jsonString = File.ReadAllText(filename);
+
+                return JsonSerializer.Deserialize<CovidConfig>(jsonString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Gagal memuat konfigurasi: {ex.Message}");
+                return new CovidConfig
+                {
+                    satuan_suhu = "Celcius",
+                    batas_hari_demam = 14,
+                    pesan_ditolak = "Akses ditolak.",
+                    pesan_diterima = "Akses diterima"
+                };
+            }
+        }
+
+    public void UbahSatuan()
+        {
+            if (this.satuan_suhu.ToLower() == "celcius")
+            {
+                this.satuan_suhu = "fahrenheit";
+            }
+            else if (this.satuan_suhu.ToLower() == "fahrenheit")
+            {
+                this.satuan_suhu = "celcius";
+            }
+        }
+    } 
 }
--- tpmodul8_103082430003/Program.cs
+++ tpmodul8_103082430003/Program.cs
@@ -1,1 +1,48 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace tpmodul8_103082430003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CovidConfig config = CovidConfig.LoadConfigFile();
+
+            config.UbahSatuan();
+
+            Console.WriteLine("=== Aplikasi Pengecekan Status Covid ===");
+            //Input 1
+            Console.Write($"Berapa suhu badan anda saat ini? dalam nilai {config.satuan_suhu} :");
+            double suhuInputan = Convert.ToDouble(Console.ReadLine());
+            //Input 2
+            Console.Write($"Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman?");
+            int inputHari = Convert.ToInt32(Console.ReadLine());
+
+            bool KondisiSuhu = false;
+            if (config.satuan_suhu.ToLower()=="celcius")
+            {
+                //Range 36.5-37.5
+                if (suhuInputan >= 36.5 && suhuInputan <= 37.5) KondisiSuhu = true;
+
+            }
+            else if (config.satuan_suhu.ToLower()=="fahrenheit")
+            {
+                //Range 97.7-99.5
+                if (suhuInputan >= 97.7 && suhuInputan <= 99.5) KondisiSuhu = true;
+            }
+
+            if (KondisiSuhu && inputHari < config.batas_hari_demam)
+            {
+                Console.WriteLine($"{config.pesan_diterima}");
+            }
+            else
+            {
+                Console.WriteLine($"{config.pesan_ditolak}");
+            }
+
+            Console.WriteLine("=== Selesai ===");
+            Console.ReadKey();
+        }
+    }
+}
